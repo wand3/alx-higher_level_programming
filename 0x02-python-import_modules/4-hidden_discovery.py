cf8aa68e471e8ce6e4d798262b9f7e7268262d69
wand3 <godswillbaiyewu@gmail.com>
2022-09-23 10:15:32 +0000

    print ASCII uppercase letters only in 3 lines

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     '''print names defined by compiled module hidden_4.pyc'''
     import hidden_4
 
-    hidden = dir(hidden_4)
-    for i in hidden:
-        if (i[:1] != '_') and (i[:2] != '__'):
-            print(hidden)
+    for i in dir(hidden_4):
+        if (i[0] != '_') and (i[1] != '_'):
+            print(i)
