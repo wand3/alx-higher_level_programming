e6f13ddca5206f9d62defe9df4ecb5ca332f9a2a
wand3 <godswillbaiyewu@gmail.com>
2022-09-27 14:50:45 +0000

    palindrome commit

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,22 @@
+#include <Python.h>
+
+/**
+* print_python_list_info - Prints basic info about Python lists
+* @p: a PyObject list
+*/
+void print_python_list_info(PyObject *p)
+{
+	int size, alloc, i;
+	PyObject *obj;
+
+	size = Py_SIZE(p);
+	alloc = ((PyListObject *)p)->allocated;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Element %d: ", i);
+
+		obj = PyList_GetItem(p, i);
+		printf("%s\n", Py_TYPE(obj)->tp_name);
+	}
+}
