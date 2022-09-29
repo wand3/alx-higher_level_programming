4f2cea3fa82408366ee66ac91b83bfc9cd7eb452
wand3 <godswillbaiyewu@gmail.com>
2022-09-29 03:37:57 +0000

    returns the greatest value of integer from a dictionary

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    """ gets the best value from a dictionary it's greatest integer value """
+    max_val = 0
+    winner = None
+    if type(a_dictionary) is dict:
+        for key, value in a_dictionary.items():
+            if value > max_val:
+                max_val = value
+                winner = key
+    return winner
