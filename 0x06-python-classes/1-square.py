fbfc584862570a4afab1d48d05762aecfeaa1519
wand3 <godswillbaiyewu@gmail.com>
2022-10-13 01:19:03 +0000

    printing a square of #

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
 """Square module.
-
 This module contains a class that defines a square and init method that
 sets its size.
-
 """
 
 
 class Square():
-     """Class Square that has attributes. Instantiation with size
-
-    Attributes:
-        size (int): The size of the square
-    """
+    """Defines a square."""
 
     def __init__(self, size):
-        """The __init__ method for Square class
-
+        """Sets the necessary attributes for the Square object.
         Args:
-            size: (:obj: 'int'): A private instance size
+            size (int): the size of one edge of the square.
         """
         self.__size = size
