cf6b4ef0ac21fc9d2c755b3ccbfc9762129f6011
root <godswillbaiyewu@gmail.com>
2022-10-26 06:34:46 -0700

    python class inheritance and instance check

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Contains definiton for the class MyList that inherits from list.
+"""
+
+
+class MyList(list):
+    """
+     class MyList that inherits from list
+    """
+    def print_sorted(self):
+        """
+        Public instance method that prints sorted list
+        """
+        list_copy = self[:]
+        list_copy.sort()
+        print(list_copy)
