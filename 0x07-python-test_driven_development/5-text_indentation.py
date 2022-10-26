446461790b8c9b0fe1fbbbae3bfbf2da7d88b0e4
root <godswillbaiyewu@gmail.com>
2022-10-26 02:35:45 -0700

    TDD functions

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This is the text_indentation module.
+This module supplies one function, text_indentation().
+"""
+
+
+
+def text_indentation(text):
+    """
+    prints a string with 2 new lines after '.', '?', and ':'
+
+    Args:
+        text (int): text to print
+
+    Raises:
+        TypeError: "text must be a string"
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    string = text.replace('.', '.\n\n').replace(':', ':\n\n')\
+        .replace('?', '?\n\n')
+    for i in range(len(text)):
+        string = string.replace('\n ', '\n')
+
+    print(string, end='')
