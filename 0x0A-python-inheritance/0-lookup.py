8fa0ceacab712646148ce7344141d40d84d0982c
root <godswillbaiyewu@gmail.com>
2022-10-26 03:12:48 -0700

    function that returns the list of available attributes and methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Created on Wednesday october 2022
+
+@author: Baiyewu Babawande
+"""
+
+
+def lookup(obj):
+    """
+    Returns all objects in an objects dictionary as a list
+    """
+    return dir(obj)
