cd970c40685a1d765a7c90713c8a20c4b77cd338
root <godswillbaiyewu@gmail.com>
2022-11-30 03:50:21 -0800

    mysql inroduction

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,2 @@
+-- create mysql database if it does not exist on server
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- script that deletes database if it exists
+DROP DATABASE IF EXISTS hbtn_0c_0;
